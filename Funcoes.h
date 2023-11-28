e994c4a60efd498a8b0b4bff4e8698222835c4ee
Crautor <vitor.dallanol@gmail.com>
2023-11-28 18:37:02 -0300

    funcionou :)

--- Funcoes.h
+++ Funcoes.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include<windows.h>
+
 struct No
     {
         int num;
@@ -100,6 +102,7 @@ int insere_ArvBin(ArvBin* raiz,int *contElite,int *cont18,int *cont19a29,int *co
         }
     }
 }
+
 void libera_NO(no *no_liberar){
     if(no_liberar == NULL){
         return;
@@ -317,7 +320,186 @@ int consulta_ArvBin(ArvBin *raiz, int valor){
     }
     return 0;
 }
+// balancear
 
+
+void converterParaLista(ArvBin *raiz, ArvBin *inicioLista) {
+    if (*raiz != NULL) {
+        converterParaLista(&((*raiz)->esq), inicioLista);
+        if (*raiz != NULL) {
+            insereMeio(inicioLista, raiz);
+        }
+        converterParaLista(&((*raiz)->dir), inicioLista);
+    }
+}
+
+ArvBin encontrarItem(ArvBin *inicioLista, int posicao) {
+    ArvBin atual = *inicioLista;
+    int contador = 0;
+
+    while (atual != NULL && contador < posicao) {
+        atual = atual->dir;
+        contador++;
+    }
+
+    ArvBin copia = (ArvBin) malloc(sizeof(struct No));
+    if (copia != NULL) {
+        copia->num = atual->num;
+    } else {
+        printf("Erro na alocacao...\n");
+        exit(0);
+    }
+
+    return copia;
+}
+
+
+
+void insereBalanceado(ArvBin *raiz, ArvBin *novo) {
+
+    (*novo)->esq = NULL;
+    (*novo)->dir = NULL;
+
+    if (*raiz == NULL) {
+        *raiz = *novo;
+    } else {
+        no *atual = *raiz;
+        no *ant = NULL;
+        while (atual != NULL) {
+            ant = atual;
+            if ((*novo)->num == atual->num) {
+                printf("Elemento ja existe...");
+                sleep(2);
+                return;
+            }
+            if ((*novo)->num > atual->num) {
+                atual = atual->dir;
+            } else {
+                atual = atual->esq;
+            }
+        }
+        if ((*novo)->num > ant->num) {
+            ant->dir = *novo;
+        } else {
+            ant->esq = *novo;
+        }
+    }
+}
+
+
+void criarArvoreBalanceada(ArvBin *novaRaiz, ArvBin *inicioLista, int inicio, int fim) {
+    if (inicio <= fim) {
+        int meio = (inicio + fim) / 2;
+
+        ArvBin meioItem = encontrarItem(inicioLista, meio);
+
+        insereBalanceado(novaRaiz, &meioItem);
+
+        criarArvoreBalanceada(novaRaiz, inicioLista, inicio, meio - 1);
+        criarArvoreBalanceada(novaRaiz, inicioLista, meio + 1, fim);
+    }
+}
+
+
+void balancearArvore(ArvBin *raiz,int *contador) {
+
+    int qtdNos = Contar_No_Chamar(raiz,contador);
+
+    ArvBin *inicioLista = (ArvBin *) malloc(qtdNos * sizeof(ArvBin));
+    if (inicioLista != NULL) {
+        *inicioLista = NULL;
+    } else {
+        printf("Erro na alocacao...\n");
+        exit(0);
+    }
+
+    converterParaLista(raiz, inicioLista);
+
+    ArvBin *novaRaiz = criarArvBin();
+
+    criarArvoreBalanceada(novaRaiz, inicioLista, 0, qtdNos - 1);
+
+    liberaLista(inicioLista);
+
+    *raiz = *novaRaiz;
+}
+
+
+void insereMeio(ArvBin *lista, ArvBin *raiz) {
+    no *novo = (no *) malloc(sizeof(no)); // Cria item
+    if (novo == NULL) {
+        exit(0);
+    }
+
+    novo->num = (*raiz)->num;
+    
+
+    if ((*lista) == NULL) { // Verifica se a lista esta vazia
+        novo->dir = (*lista); // Novo item da lista recebe o apontamento do início da lista
+        *lista = novo; // Apontamento inicial da lista recebe o endereço do novo item
+        novo->esq = NULL; // Apontamento anterior do novo item recebe endereço de NULL, pois é o início da lista
+        return;
+    } else {
+        no *tmp;
+        tmp = *lista;
+
+        // Caso em que a lista só tem um item
+        if ((*lista) != NULL && tmp->dir == NULL) { // Verifica se a lista tem apenas um item
+            if (tmp->num > novo->num) { // Se o único item for maior que o novo
+                novo->dir = tmp;  // Novo->próximo recebe endereço do único item
+                *lista = novo; // Apontamento inicial da lista recebe o endereço do novo item
+                novo->dir->esq = novo; // Antigo primeiro item que já existia recebe o endereço anterior o item novo
+                novo->esq = NULL; // Apontamento anterior do novo item recebe endereço de NULL, pois é o início da lista
+            } else {
+                novo->dir = NULL; // Novo->próximo recebe endereço NULL
+                tmp->dir = novo; // Único item recebe endereço do novo
+                novo->esq = tmp; // Apontamento do anterior do novo item recebe o antigo último
+            }
+            return;
+        }
+
+        // Caso para inserir no meio
+        do {
+            if (tmp->num > novo->num) { // Se o número for menor que o primeiro da lista
+                novo->dir = tmp; // Novo->próximo recebe endereço do único item
+                tmp->esq = novo;
+                novo->esq = NULL;
+                *lista = novo; // Ponteiro inicial aponta para novo
+                return;
+            }
+            if (tmp->num < novo->num &&
+                tmp->dir->num > novo->num) { // Se o número for maior que o atual e menor que o próximo
+                novo->dir = tmp->dir;
+                novo->esq = tmp;
+                tmp->dir->esq = novo;
+                tmp->dir = novo;
+                return;
+            }
+            tmp = tmp->dir;
+            if (tmp->num < novo->num &&
+                tmp->dir == NULL) { // Se o número for maior que o atual e for o último da lista
+                novo->dir = NULL;
+                tmp->dir = novo;
+                novo->esq = tmp;
+                return;
+            }
+        } while (tmp->dir != NULL); //Repete enquanto não entrar em nenhuma das condições
+    }
+}
+
+
+void liberaLista(ArvBin *lista) {
+    if ((*lista) == NULL) {
+        return;
+    } else {
+        no *tmp;
+        while ((*lista) != NULL) {
+            tmp = *lista;
+            *lista = (*lista)->dir;
+            free(tmp);
+        }
+    }
+}
 
 void MenuInsercao(int *opcao){
         printf("\n====================================\n");
@@ -333,6 +515,7 @@ void MenuInsercao(int *opcao){
         scanf("%d", opcao);
 
 }
+
 void MenuPrincipal(int *opcao){
         printf("\n====================================\n");
         printf("MENU\n");
@@ -345,6 +528,8 @@ void MenuPrincipal(int *opcao){
         printf("7 - Altura Arvore\n");
         printf("8 - Consultar Elemento\n");
         printf("9 - Remover Elemento\n");
+        printf("10 - Atualizar Elemento\n");
+        printf("11 - Balancear Arvore\n");
         printf("0 - Sair\n");       
         printf("====================================\n");
         printf("Digite a Opcao: ");
--- Main.c
+++ Main.c
@@ -6,43 +6,63 @@
 
 int main() {
     no *raiz = criarArvBin();
-    int opcao,valor,contElite=1,cont18=101,cont19a29=201,cont30a39=301,cont40=401,contador,alt_dir=0,alt_esq=0;
+    int opcao,valor,contElite=1,cont18=101,cont19a29=201,cont30a39=301,cont40=401,contador=0;
     do{
         MenuPrincipal(&opcao);
         
 
         switch (opcao){
             case 1:
+                // insert
                 insere_ArvBin(raiz,&contElite,&cont18,&cont19a29,&cont30a39,&cont40);
                 break;
-           case 2:
+            case 2:
+                // pre ordem
                 Pre_Ordem_chamar(raiz);
                 break;  
             case 3:
+                // in ordem
                 In_Ordem_Chamar(raiz);
                 break;
             case 4:
+                // pos ordem
                 Pos_Ordem_chamar(raiz);
                 break;
             case 5:
+                // contar no
                 Contar_No_Chamar(raiz,&contador);
                 printf("%d\n", contador);
                 break;
             case 6:
+                // liberar
                 libera_ArvBin(raiz);
                 break;
             case 7:
+                // altura
                 printf("\n %d \n ",altura_ArvBin(raiz)); 
                 break;
             case 8:
+                // read
                 printf("informe o numero a ser consultado: ");
                 scanf("%d",&valor);
                 consulta_ArvBin(raiz,valor);
                 break;
             case 9:
+                // remove
                 printf("informe o numero a ser removido: ");
                 scanf("%d",&valor);
                 remove_ArvBin(raiz,valor);
+                break;
+            case 10:
+                // update
+                printf("informe o numero a ser atualizado: ");
+                scanf("%d",&valor);
+                remove_ArvBin(raiz,valor);
+                insere_ArvBin(raiz,&contElite,&cont18,&cont19a29,&cont30a39,&cont40);
+                break;
+
+            case 11:
+                balancearArvore(raiz,&contador);
                 break;
             case 0:
                 printf("Voce decidiu parar!");
